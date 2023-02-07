870ef24996068cd8848385bce06aa871d143aa5f
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-02-07 09:53:27 -0700

    MVC folders created.

--- src/main/java/bean/vanilla/casinosim/Controller/HelloController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package bean.vanilla.casinosim;
+package bean.vanilla.casinosim.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module bean.vanilla.casinosim {
 
     opens bean.vanilla.casinosim to javafx.fxml;
     exports bean.vanilla.casinosim;
+    exports bean.vanilla.casinosim.Controller;
+    opens bean.vanilla.casinosim.Controller to javafx.fxml;
 }
