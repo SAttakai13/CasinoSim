bf62b18c0b877b4b4c1836053eb246e4dda1044a
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-02-08 10:18:07 -0700

    Set up for balance class and hand class, there is an error in the hand class that needs to be changed

--- src/main/java/bean/vanilla/casinosim/Model/Hand.java
+++ src/main/java/bean/vanilla/casinosim/Model/Hand.java
@@ -1,4 +1,74 @@
 package bean.vanilla.casinosim.Model;
 
-public class Hand {
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
+
+public class Hand extends StackPane {
+    //public ArrayList<Card> cards;
+
+
+    public Hand() {
+        /*cards = new ArrayList<>();*/
+        //this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+        this.setAlignment(Pos.TOP_CENTER);
+    }
+    public void removeOneFromHand(Card value){
+        getChildren().remove(value);
+        this.setLayoutY(0);
+    };
+
+    public void addToHand(Card card) {
+        card.setPadding(new Insets(getChildren().size()*10, 0, 0, getChildren().size()*40));
+        card.setLayoutY(0);
+        getChildren().add(card);
+    }
+
+    public void resetHand(){
+        getChildren().clear();
+    }
+
+    public int Points() {
+        int totalValue = 0;
+        int aceCount = 0;
+
+        for (Node card : getChildren()) {
+            if (card instanceof Card) {
+                int value = 0;
+                String cardCharacter = ((Card)card).GetCardCharacter();
+                try {
+                    value = Integer.valueOf(cardCharacter);
+                } catch (NumberFormatException e) {
+                    switch (cardCharacter) {
+                        case "J", "Q", "K" -> value = 10;
+                        case "A" -> {
+                            value = 11;
+                            aceCount++;
+                        }
+                    }
+                }
+                totalValue += value;
+            }
+        }
+
+        if (totalValue > 21) {
+            for (int i = 0; i < aceCount; i++) {
+                totalValue -= 10;
+                if (totalValue <= 21)
+                    break;
+            }
+        }
+        return totalValue;
+    }
+
+
+
+
 }
