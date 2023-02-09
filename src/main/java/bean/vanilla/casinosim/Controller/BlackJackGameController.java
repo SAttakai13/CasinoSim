3bd4e5cd43b94aa1fbefe07f5b76905eefdb97e3
JacobMabey <mabey.jacob@gmail.com>
2023-02-09 15:17:49 -0700

    Finished the game controller without button logic.

--- src/main/java/bean/vanilla/casinosim/Controller/BlackJackGameController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/BlackJackGameController.java
@@ -17,8 +17,11 @@ public class BlackJackGameController implements Initializable {
 
     private Player dealer;
     private Deck deck;
+    private Color deckColor = Color.BLUE;
 
     private boolean isPlayersTurn;
+
+    private double betAmount = 100.0;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -30,7 +33,7 @@ public class BlackJackGameController implements Initializable {
         CasinoApplication.player.playerHand.SetPosition(200, 300);
         CasinoApplication.player.playerHand.Scale(1.0/3.0);
         pane.getChildren().addAll(dealer.playerHand, CasinoApplication.player.playerHand);
-        deck = new Deck(Color.BLUE);
+        deck = new Deck(deckColor);
         DealCards();
 
         dealer.playerHand.FlipCard(0);
@@ -45,5 +48,70 @@ public class BlackJackGameController implements Initializable {
         deck.DealCard(dealer);
         deck.DealCard(CasinoApplication.player);
         deck.DealCard(dealer);
+    }
+
+    private void ExchangeBet(Player sender, Player receiver) { ExchangeBet(sender, receiver, 1.0); }
+    private void ExchangeBet(Player sender, Player receiver, double multiplier) {
+        receiver.GetBalance().AddToBalance(betAmount * multiplier);
+        sender.GetBalance().LostPartBalance(betAmount * multiplier);
+    }
+
+
+    private void EndPlayerTurn() {
+        //Dealer AI
+        int dealerPoints = dealer.playerHand.BlackJackPoints();
+        if (dealerPoints < 17) deck.DealCard(dealer);
+
+        DetermineWinner();
+    }
+
+
+    private void NewRound() {
+        dealer.playerHand.resetHand();
+        CasinoApplication.player.playerHand.resetHand();
+
+        deck = new Deck(deckColor);
+        DealCards();
+        isPlayersTurn = true;
+    }
+
+
+    private void DetermineWinner() {
+        int dealerPoints = dealer.playerHand.BlackJackPoints();
+
+        //if dealer busts all players win
+        if (dealerPoints > 21) {
+            ExchangeBet(dealer, CasinoApplication.player, 2.0);
+        }
+        else {
+            int points = CasinoApplication.player.playerHand.BlackJackPoints();
+
+            if (points == dealerPoints) {
+                //tied
+                System.out.println(CasinoApplication.player.GetName() + " Tied with the Dealer!");
+
+                //Return bets
+                ExchangeBet(dealer, CasinoApplication.player);
+
+            } else if (points <= 21 && points > dealerPoints) {
+                //WIN
+                System.out.println(CasinoApplication.player.GetName() + " : "+ points + " wins! Against Dealer: "+dealerPoints);
+
+
+                //Adjust balance
+                if (points == 21) {
+                    ExchangeBet(dealer, CasinoApplication.player, 3.0);
+                    System.out.println(CasinoApplication.player.GetName() + " BlackJack!");
+                } else {
+                    ExchangeBet(dealer, CasinoApplication.player, 2.0);
+                }
+            } else {
+                //Lose
+                System.out.println(CasinoApplication.player.GetName() + " Lost! Against Dealer: "+dealerPoints);
+            }
+        }
+
+        //Start new Round
+        NewRound();
     }
 }
