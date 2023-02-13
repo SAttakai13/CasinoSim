6ade4499811fe7e0c73938e83ac288a8a8d93aaf
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-02-13 10:27:38 -0700

    Cringe

--- src/main/java/bean/vanilla/casinosim/Controller/TitleScreenController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/TitleScreenController.java
@@ -1,5 +1,6 @@
 package bean.vanilla.casinosim.Controller;
 
+import bean.vanilla.casinosim.CasinoApplication;
 import bean.vanilla.casinosim.Model.Card;
 import bean.vanilla.casinosim.Model.Deck;
 import bean.vanilla.casinosim.Model.Dice;
@@ -8,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -15,6 +17,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TitleScreenController {
+    @FXML
+    public ImageView BlackJackScreen;
+
+    public void NavToBlackJack(){
+        CasinoApplication.setRoot("BlackJackGameScreen");
+    }
+
 
 
 }
