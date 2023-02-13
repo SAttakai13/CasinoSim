d63950465b9c23efdd65e39634815d8bd86f514a
JacobMabey <mabey.jacob@gmail.com>
2023-02-13 10:16:12 -0700

    Made Main application file so we can switch scenes easily

--- src/main/java/bean/vanilla/casinosim/Controller/PokerGameController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/PokerGameController.java
@@ -1,4 +1,23 @@
 package bean.vanilla.casinosim.Controller;
 
-public class PokerGameController {
+import bean.vanilla.casinosim.Model.Deck;
+import javafx.fxml.Initializable;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PokerGameController implements Initializable {
+
+
+    private Deck deck;
+    private double betAmount = 100.0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        deck = new Deck(Color.RED);
+
+    }
+
+
 }
