1d835355bbbac232b3c11c6b60e1ac828a030a91
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-02-13 12:04:46 -0700

    Adding in title screen nav buttons

--- src/main/java/bean/vanilla/casinosim/Controller/TitleScreenController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/TitleScreenController.java
@@ -5,11 +5,15 @@ import bean.vanilla.casinosim.Model.Card;
 import bean.vanilla.casinosim.Model.Deck;
 import bean.vanilla.casinosim.Model.Dice;
 import bean.vanilla.casinosim.Model.Player;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
@@ -17,13 +21,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TitleScreenController {
-    @FXML
-    public ImageView BlackJackScreen;
 
-    public void NavToBlackJack(){
+    public void NavToBlackjack(MouseEvent event) {
         CasinoApplication.setRoot("BlackJackGameScreen");
     }
 
+    public void NavToPoker(MouseEvent event) {
+        CasinoApplication.setRoot("PokerGameScreen");
+    }
 
+    public void NavToRoulette(MouseEvent event) {
+        CasinoApplication.setRoot("RouletteGameScreen");
+    }
 
+    public void NavToSlots(MouseEvent event) {
+        CasinoApplication.setRoot("SlotsGameScreen");
+    }
+
+    public void NavToCraps(MouseEvent event) {
+        CasinoApplication.setRoot("CrapsGameScreen");
+    }
+
+    public void NavToCredit(MouseEvent event) {
+        //Need a credits screen
+    }
+
+    public void Quit(MouseEvent event) {
+        Platform.exit();
+    }
 }
