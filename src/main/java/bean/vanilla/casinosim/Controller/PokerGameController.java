e58e54f039efd7af5b9a0a4042e5407ebec4b46d
JacobMabey <mabey.jacob@gmail.com>
2023-02-15 09:58:26 -0700

    Started on poker controller

--- src/main/java/bean/vanilla/casinosim/Controller/PokerGameController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/PokerGameController.java
@@ -1,13 +1,22 @@
 package bean.vanilla.casinosim.Controller;
 
+import bean.vanilla.casinosim.CasinoApplication;
 import bean.vanilla.casinosim.Model.Deck;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PokerGameController implements Initializable {
+
+    @FXML
+    private Text BetText;
+    @FXML
+    private Text BalText;
 
 
     private Deck deck;
@@ -17,7 +26,46 @@ public class PokerGameController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         deck = new Deck(Color.RED);
 
+
     }
 
 
+
+    public void Raise(MouseEvent event) {
+
+    }
+    public void Call(MouseEvent event) {
+
+    }
+    public void Fold(MouseEvent event) {
+
+    }
+
+
+
+    public void GoBackToMain(MouseEvent event) {
+        CasinoApplication.setRoot("TitleScreen");
+    }
+
+    public void updateBetsAndBalance(double bets, double playerBalance){
+        BetText.setLayoutX(914.0);
+        BetText.setLayoutY(794.0);
+
+        BalText.setLayoutX(860.0);
+        BalText.setLayoutY(836.0);
+
+        BetText.setText("Bet: " + bets);
+        BalText.setText("Balance: " + playerBalance);
+    }
+
+
+    public void DecreaseBet(MouseEvent event) {
+        betAmount -= 50;
+        updateBetsAndBalance(betAmount, CasinoApplication.player.GetBalance().GetBalance());
+    }
+
+    public void IncreaseBet(MouseEvent event) {
+        betAmount += 50;
+        updateBetsAndBalance(betAmount, CasinoApplication.player.GetBalance().GetBalance());
+    }
 }
