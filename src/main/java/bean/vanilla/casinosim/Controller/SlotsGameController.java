e0b6b2a5d7fda49575582df0ba98b833d2de9eb6
Puzzle-Queen <81598037+SAttakai13@users.noreply.github.com>
2023-02-27 13:42:19 -0700

    The poker game should be fixed now

--- src/main/java/bean/vanilla/casinosim/Controller/SlotsGameController.java
+++ src/main/java/bean/vanilla/casinosim/Controller/SlotsGameController.java
@@ -1,4 +1,47 @@
 package bean.vanilla.casinosim.Controller;
 
-public class SlotsGameController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class SlotsGameController implements Initializable {
+
+    @FXML
+    private Pane pane;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+
+
+
+    public void SpinWheel(){
+
+
+    }
+
+
+    public void Quit(MouseEvent event) {
+    }
+
+    public void DecreaseBet(MouseEvent event) {
+    }
+
+    public void Spin(MouseEvent event) {
+    }
+
+    public void IncreaseBet(MouseEvent event) {
+    }
 }
--- src/main/java/bean/vanilla/casinosim/Model/SlotsWheels.java
+++ src/main/java/bean/vanilla/casinosim/Model/SlotsWheels.java
@@ -1,4 +1,36 @@
 package bean.vanilla.casinosim.Model;
 
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SlotsWheels {
+
+    public List<Image> slotImages = new ArrayList<>();
+
+    public List<Image> fillWheel(){
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Banana.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Bars.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Bell.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Cherry.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Lemon.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Melon.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Orange.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Plum.png"));
+        slotImages.add(new Image("src/main/resources/CasinoAssets/Slots/Wheel - Seven.png"));
+
+        return slotImages;
+    }
+
+    public List<Image> RandomImages(){
+        for (int i = 0; i < slotImages.size(); i++){
+            slotImages.get(i);
+        }
+        return null;
+    }
+
+
+
+
 }
